8e9eda4d36c4def599c11aee0342fa5aa89d646b
Kcantes <chevaliq2@gmail.com>
2020-11-20 16:05:40 +0100

    limit

--- src/app.service.ts
+++ src/app.service.ts
@@ -26,28 +26,32 @@ export class AppService {
 
   async getFilms(queryElements): Promise<Film[]> {
     let regexAuthor = {};
-    if ("author" in queryElements)
+    if ("author" in queryElements && queryElements["author"] != "")
       regexAuthor = { $or: [{ authors: { $regex: queryElements["author"], $options: 'i' } }, { casting: { $regex: queryElements["author"], $options: 'i' } }], }
     let regexSearch = {};
-    if ("search" in queryElements)
+    if ("search" in queryElements && queryElements["search"] != "")
       regexSearch = { $or: [{ authors: { $regex: queryElements["search"], $options: 'i' } }, { casting: { $regex: queryElements["search"], $options: 'i' } }, { title: { $regex: queryElements["search"], $options: 'i' } }, { genres: { $regex: queryElements["search"], $options: 'i' } }, { keywords: { $regex: queryElements["search"], $options: 'i' } }], }
     let regexGenre = {};
-    if ("genre" in queryElements)
+    if ("genre" in queryElements && queryElements["genre"] != "")
       regexGenre = { genres: { $regex: queryElements["genre"], $options: 'i' } }
     let regexFinal = { $and: [regexAuthor, regexSearch, regexGenre] }
-    if ("sort-by" in queryElements) {
+    let films = this.FilmModel.find(regexFinal)
+    let mysort:any
+    if ("sort-by" in queryElements && queryElements["sort-by"] != "") {
       let ascen = 1;
-      if ("asc" in queryElements)
+      if ("asc" in queryElements && queryElements["asc"] != "")
         if (queryElements["asc"] == "-1")
           ascen = -1;
-      let mysort = {[queryElements["sort-by"]]:ascen};
-      let films = this.FilmModel.find(regexFinal).sort(mysort).exec();
-      return films;
+      mysort = { [queryElements["sort-by"]]: ascen }
     }
-    else {
-      let films = this.FilmModel.find(regexFinal).sort({ "first_release_date": -1 }).exec();
-      return films;
+    else{
+      mysort = { "first_release_date": -1 }
     }
+    films=films.sort(mysort)
+    if ("limit" in queryElements && queryElements["limit"] != "" && queryElements["limit"] != 0 && queryElements["limit"] != "0") {
+      films = films.limit(queryElements["limit"])
+    }
+    return films.exec();
   }
 
   async getGenres(): Promise<any> {
