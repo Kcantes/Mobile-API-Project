a5a3363b1360a91c124f79538a975640ae09f604
Kcantes <chevaliq2@gmail.com>
2020-11-30 21:21:37 +0100

    delete return json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -58,7 +58,7 @@ export class AppController {
   @Delete(':film')
   deleteFilm(@Param() film) {
     this.appService.deleteFilm(decodeURIComponent(film["film"]))
-    return film["film"]+"deleted"
+    return "{ 'film':"+"'"+film["film"]+"deleted'}"
   }
 
 }
